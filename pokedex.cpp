84d2414c39ba27843e3631356756dd653ecfeaa9
Guilherme Cotta Marques Ribeiro <guilhermecottamr@gmail.com>
2023-11-10 13:56:19 -0300

    Trabalho Pokedex nao comentado

--- pokedex.cpp
+++ pokedex.cpp
@@ -1,34 +1,35 @@
 #include<iostream>
-#include<list>
 #include<string>
 
 using namespace std;
 
+string vet_tipos[] = {"Agua", "Fogo", "Normal"};
+
 struct treenode{
 	int numero_id;
     string nome;
-    string tipo;
+    int tipo;
     string pokebola;
 	treenode *esq;
 	treenode *dir;
 };
 typedef treenode *treenodeptr;
 
-bool ehMaior(string s1, string s2) {
-    for(int i = 0; i < s1.size() && i < s2.size(); i++){
+bool nome_maior(string s1, string s2) {
+    if(s1.size() >= s2.size()){
+        return 1;
+    }
+	for(int i = 0; i < s1.size() && i < s2.size(); i++){
         if(s1[i] > s2[i]){
             return 1;
         }else if(s1[i] < s2[i]){
             return 0;
         }
     }
-    if(s1.size() >= s2.size()){
-        return 1;
-    }
     return 0;
 }
 
-void insert(treenodeptr &arvore, int num, string nome, string tipo, string pokebola){
+void insert_nome(treenodeptr &arvore, int num, string nome, int tipo, string pokebola){
 	if(arvore == NULL){
 		arvore = new treenode;
 		arvore -> numero_id = num;
@@ -37,128 +38,264 @@ void insert(treenodeptr &arvore, int num, string nome, string tipo, string pokeb
         arvore -> pokebola = pokebola;
 		arvore -> esq = NULL;
 		arvore -> dir = NULL;
-	}else if(ehMaior(arvore -> nome, nome)){
-		insert(arvore -> esq, num, nome, tipo, pokebola);
+	}else if(nome_maior(arvore -> nome, nome)){
+		insert_nome(arvore -> esq, num, nome, tipo, pokebola);
     }
 	else{
-		insert(arvore -> dir, num, nome, tipo, pokebola);
+		insert_nome(arvore -> dir, num, nome, tipo, pokebola);
     }
 }
 
-treenodeptr search(treenodeptr pesq, string nome){
+void insert_tipo(treenodeptr &arvore, int num, string nome, int tipo, string pokebola){
+	if(arvore == NULL){
+		arvore = new treenode;
+		arvore -> numero_id = num;
+        arvore -> nome = nome;
+        arvore -> tipo = tipo;
+        arvore -> pokebola = pokebola;
+		arvore -> esq = NULL;
+		arvore -> dir = NULL;
+	}else if(tipo < arvore -> tipo){
+		insert_tipo(arvore -> esq, num, nome, tipo, pokebola);
+    }
+	else{
+		insert_tipo(arvore -> dir, num, nome, tipo, pokebola);
+    }
+}
+
+treenodeptr search_nome(treenodeptr pesq, string nome){
 	if(pesq == NULL){
 		return NULL;
     }else if(nome.compare(pesq -> nome) == 0){
 		return pesq;
-    }else if(ehMaior(pesq -> nome, nome)){
-		return search(pesq -> esq, nome);
+    }else if(nome_maior(pesq -> nome, nome)){
+		return search_nome(pesq -> esq, nome);
     }else{
-		return search(pesq -> dir, nome);
+		return search_nome(pesq -> dir, nome);
     }
 }
 
-/*
-treenodeptr menor(treenodeptr &raiz){
-	treenodeptr t;
-	t = raiz;
-	if (t->esq == NULL){
-		raiz = raiz->dir;
-		return t;
-	}else{
-		return menor(raiz->esq);
+void imprimir(treenodeptr arvore) {
+    if (arvore != NULL) {
+        imprimir(arvore->esq);
+        cout << endl << "Numero: " << arvore->numero_id << endl << "Nome: " << arvore->nome << endl << "Tipo: " 
+			<< vet_tipos[arvore->tipo] << endl << "Pokebola: " << arvore->pokebola << endl;
+        imprimir(arvore->dir);
     }
 }
 
-int remove(treenodeptr &arvore, int num){
-	treenodeptr p;
-	if (arvore == NULL){
-		return 1;
-    }
-	if (num == arvore->numero_id){
-		p = arvore;
-		if(arvore->esq == NULL){
-			arvore = arvore->dir;
-        }else if(arvore->dir == NULL){
-			arvore = arvore->esq;
-        }else{
-			p = menor(arvore->dir);
-			arvore->numero_id = p->numero_id;
-		}
-		delete p;
-		return 0;
-	}else if(num < arvore->numero_id){
-		return remove(arvore->esq, num);
-    	}else{
-		return remove(arvore->dir, num);
+void imprimir_pokebola(treenodeptr arvore, string pokebola) {
+    if (arvore != NULL) {
+        imprimir_pokebola(arvore->esq, pokebola);
+        if(arvore->pokebola.compare(pokebola) == 0)
+			cout << endl << "Numero: " << arvore->numero_id << endl << "Nome: " << arvore->nome << endl << "Tipo: " 
+				<< vet_tipos[arvore->tipo] << endl << "Pokebola: " << arvore->pokebola << endl;
+        imprimir_pokebola(arvore->dir, pokebola);
     }
 }
 
-void emNivel(treenodeptr t){
-	treenodeptr n;
-	list<treenodeptr> q;
+treenodeptr remove_pokebola(treenodeptr raiz, string pokebola) {
+    if (raiz == NULL) {
+        return raiz;
+    }
 
-	if(t != NULL){
-		q.push_back(t);
-		while(!q.empty()){
-			n = *q.begin();
-			q.pop_front();
-			if(n->esq != NULL)
-				q.push_back(n->esq);
-			if(n->dir != NULL)
-				q.push_back(n->dir);
-			cout << n->numero_id << " ";
-		}
-	}
+    if (pokebola < raiz->pokebola) {
+        raiz->esq = remove_pokebola(raiz->esq, pokebola);
+    } else if (pokebola > raiz->pokebola) {
+        raiz->dir = remove_pokebola(raiz->dir, pokebola);
+    } else {
+        
+        if (raiz->esq == NULL) {
+            treenodeptr temp = raiz->dir;
+            delete raiz;
+            return temp;
+        } else if (raiz->dir == NULL) {
+            treenodeptr temp = raiz->esq;
+            delete raiz;
+            return temp;
+        }
+
+        treenodeptr aux;
+        while (aux -> esq != NULL) {
+                aux = aux -> esq;
+            }
+        treenodeptr temp = aux;
+
+        raiz->numero_id = temp->numero_id;
+        raiz->nome = temp->nome;
+        raiz->tipo = temp->tipo;
+        raiz->pokebola = temp->pokebola;
+
+        raiz->dir = remove_pokebola(raiz->dir, temp->pokebola);
+    }
+    return raiz;
 }
-*/
+
+// Função auxiliar para encontrar primeiro por número
+bool encontrar_primeiro_aux(treenodeptr raiz, int numero, bool &encontrou) {
+    if (raiz == NULL) {
+        return false;
+    }
+
+    // Pré-Ordem
+    if (raiz->numero_id == numero) {
+        encontrou = true;
+        return true;
+    }
+    bool esquerda = encontrar_primeiro_aux(raiz->esq, numero, encontrou);
+    if (encontrou) {
+        return true;
+    }
+
+    // Em Ordem
+    bool meio = (raiz->numero_id == numero);
+    if (meio) {
+        encontrou = true;
+        return true;
+    }
+
+    bool direita = encontrar_primeiro_aux(raiz->dir, numero, encontrou);
+    if (encontrou) {
+        return true;
+    }
+
+    // Pós-Ordem
+    return esquerda || meio || direita;
+}
+
+// Função para encontrar primeiro por número
+void encontrar_primeiro(treenodeptr raiz, int numero) {
+    bool encontrou = false;
+    if (encontrar_primeiro_aux(raiz, numero, encontrou)) {
+        cout << "O número " << numero << " foi encontrado primeiro na ordem: ";
+        if (raiz->numero_id == numero) {
+            cout << "Pré-Ordem";
+        } else if (encontrou) {
+            cout << "Em Ordem";
+        } else {
+            cout << "Pós-Ordem";
+        }
+        cout << endl;
+    } else {
+        cout << "O Pokemon de numero " << numero << " nao foi encontrado na pokedex." << endl;
+    }
+}
 
 int main(){
-	treenodeptr arvore = NULL;
+	treenodeptr arvore_nome = NULL;
+	treenodeptr arvore_tipo = NULL;
 	treenodeptr pesq;
-    int opcao = 0;
+
+    int opcao = -1;
+
+	int count_agua = 0;
+	int count_fogo = 0;
+	int count_normal = 0;
+
+	string pesq_pokebola;
+	string rem_pokebola;
+	int pesq_numero;
+
 	int num = 0;
     string nome;
     string tipo;
+	int num_tipo;
     string pokebola;
 
-	cin >> opcao;
-
     while(opcao != 0){
-        switch(opcao){
+        
+		cout << endl << "Menu:" << endl; //V
+		cout << "1 - Inserir Pokemon." << endl; //V
+		cout << "2 - Pesquisar Pokemon." << endl; //V
+		cout << "3 - Remover Pokemon pela sua pokebola." << endl; //V
+		cout << "4 - Imprimir informacoes dos Pokemons cadastrados em ordem alfabetica dos nomes." << endl; //V
+		cout << "5 - Imprimir informacoes dos Pokemons cadastrados em ordem alfabetica dos tipos." << endl; //V
+		cout << "6 - Imprimir informacoes dos Pokemons cadastrados de uma dada pokebola." << endl; //V
+		cout << "7 - Imprimir informacoes sobre a quantidade de Pokemons de cada tipo." << endl; //V
+		cout << "8 - Determinar o percurso que encontra primeiro o Pokemon pelo numero." << endl; //P
+		cout << "0 - Sair." << endl << endl; //V
+
+		cin >> opcao;
+		
+		switch(opcao){
             case 1:
                 cin >> num;
                 cin >> nome;
                 cin >> tipo;
                 cin >> pokebola;
-                insert(arvore,num,nome,tipo,pokebola);
+				for(int i = 0; i < 3; i++){
+					if(tipo.compare(vet_tipos[i]) == 0){
+						num_tipo = i;
+						break;
+					}
+					cout << "Tipo inválido! Foi adicionado o tipo normal." << endl;
+					num_tipo = 2;
+				}
+				if(num_tipo == 0)
+					count_agua++;
+				else if(num_tipo == 1)
+					count_fogo++;
+				else if(num_tipo == 2)
+					count_normal++;
+                insert_nome(arvore_nome,num,nome,num_tipo,pokebola);
+				insert_tipo(arvore_tipo,num,nome,num_tipo,pokebola);
                 break;
             
             case 2:
                 cin >> nome;
-                pesq = search(arvore, nome);
+                pesq = search_nome(arvore_nome, nome);
                 if(pesq == NULL){
-                    cout << "Pokemon nao encontrado!";
+                    cout << "Pokemon nao encontrado!" << endl;
                 }else{
-                    cout << "Pokemon encontrado!";
+                    cout << "Pokemon encontrado!" << endl;
                 }
                 break;
 
+			case 3:
+				cout << "Digite a pokebola dos Pokemons a seres apagados: ";
+				cin >> rem_pokebola;
+				remove_pokebola(arvore_nome, rem_pokebola);
+				remove_pokebola(arvore_tipo, rem_pokebola);
+				cout << "Pokemons removidos com sucesso!";
+				break;
+
+			case 4:
+				cout << "Pokemons por ordem alfabética dos nomes:" << endl;
+				imprimir(arvore_nome);
+				break;
+
+			case 5:
+				cout << "Pokemons por ordem alfabética dos tipos:" << endl;
+				imprimir(arvore_tipo);
+				break;
+
+			case 6:
+				cout << "Digite a pokebola a ser pesquisada: ";
+				cin >> pesq_pokebola;
+				cout << "Pokemons que utilizam a pokebola " << pesq_pokebola << ":";
+				imprimir_pokebola(arvore_nome, pesq_pokebola);
+				break;
+
+			case 7:
+				cout << "Quantidade de Pokemons por tipos:" << endl << "Agua: " << count_agua << endl << 
+				"Fogo: "<< count_fogo << endl << "Normal: " << count_normal << endl;
+				break;
+
+			case 8:
+				cout << "Digite o numero a ser pesquisado: ";
+				cin >> pesq_numero;
+				encontrar_primeiro(arvore_nome, pesq_numero);
+				break;
+
+			case 0:
+				cout << endl << "Desligando Pokedex..." << endl;
+				break;
+
             default:
-                cout << "Opcao Inválida";
+                cout << "Opcao Invalida!" << endl;
                 break;
         }
-        cin >> opcao;
     }
-
-    /*
-	cout << "Em nivel\n";
-	emNivel(arvore);
-
-    remove(arvore,num);
-	
-	cout << "\nDepois do remove\n";
-    emNivel(arvore);
-    */
 
     return 0;
 }
